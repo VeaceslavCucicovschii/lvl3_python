b0be0321cb715be7581414d461886208d69073e3
VeaceslavCucicovschii <104380929+VeaceslavCucicovschii@users.noreply.github.com>
2023-03-04 12:18:45 +0200

    Update class.py

--- recapitulation/function_additional/class.py
+++ recapitulation/function_additional/class.py
@@ -1,14 +1,8 @@
 class Container:
-    is_empty = True
-    value = None
 
-    def setContainerValue(self, value):
-        self.value = value
-        self.is_empty = False
-    
-    def unsetContainerValue(self):
-        self.value = None
+    def __init__(self):
         self.is_empty = True
+        self.value = None
 
     def __str__(self):
         if self.is_empty == True:
@@ -16,7 +10,15 @@ class Container:
         else:
             return f"Container<{self.value}>"
 
+def setContainerValue(container, value):
+    container.is_empty = False
+    container.value = value
+    
+def unsetContainerValue(container):
+    container.is_empty = True
+    container.value = None
+
 container_0 = Container()
-container_0.setContainerValue(10)
+setContainerValue(container_0, 10)
 
 print(container_0)
